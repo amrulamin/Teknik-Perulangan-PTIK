2c8d52bdad48c0998e4e37d0def2dd20f0392645
amrulamin <55226663+amrulamin@users.noreply.github.com>
2019-10-17 12:59:31 +0700

    Create for.cpp

---
+++ for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namesace std;
+
+int main ()
+ {
+      For(int i = 0; i < 10; i++){
+          cout << "emrograman Dasar Menggunakan C++!"<< endl ;
+            }
+            return 0;
+            }
+            
